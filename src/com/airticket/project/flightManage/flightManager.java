50a0976e3059671f9593c240cefe007f65175232
Na Na sociu <16521490@gm.uit.edu.vn>
2018-10-22 20:15:00 +0700

    login, usermanage, flightmanage

--- src/com/airticket/project/flightManage/flightManager.java
+++ src/com/airticket/project/flightManage/flightManager.java
@@ -1,4 +1,15 @@
 package com.airticket.project.flightManage;
 
+import javax.swing.*;
+
 public class flightManager {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton searchButton;
+    private JButton resetButton;
+    private JButton insertButton;
+    private JButton updateButton;
+    private JTable table1;
+    private JButton deleteButton1;
 }
