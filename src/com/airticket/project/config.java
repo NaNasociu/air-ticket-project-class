efb912abfd0a774c15ed369020a0f09e15778830
John Michael <ducnm.john98@gmail.com>
2018-11-12 14:23:07 +0700

    Install db

---
+++ src/com/airticket/project/config.java
@@ -0,0 +1,11 @@
+package com.airticket.project;
+
+public class config {
+    // JDBC driver name and database URL
+    public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://103.1.236.253:3306/airTicketProject";
+
+    //  Database credentials
+    public static final String USER = "airTicketProject";
+    public static final String PASS = "airTicketProjectPass";
+}
