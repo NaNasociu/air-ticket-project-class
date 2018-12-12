099e8bae9879ee6d1a80911fa134b68ccf4d3923
Tan Nguyen <tandeptrai98dl@gmail.com>
2018-12-12 08:40:31 +0700

    Update 12-12

--- src/com/airticket/project/GUI/Menu/frmMainPage.java
+++ src/com/airticket/project/GUI/Menu/frmMainPage.java
@@ -1,6 +1,7 @@
 package com.airticket.project.GUI.Menu;
 
 import com.airticket.project.GUI.Booking.frmBooking;
+import com.airticket.project.GUI.Flight.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -22,7 +23,7 @@ public class frmMainPage extends JFrame implements ActionListener {
 
         //create label
         btn1 = createJButton("Booking", Color.red, Color.pink);
-        btn2 = createJButton("Function 2", Color.red, Color.yellow);
+        btn2 = createJButton("Airport", Color.red, Color.yellow);
         btn3 = createJButton("Function 3", Color.red, Color.cyan);
         btn4 = createJButton("Function 4", Color.red, Color.gray);
         JLabel lb5 = new JLabel("icon", icon, JLabel.CENTER);
@@ -55,8 +56,15 @@ public class frmMainPage extends JFrame implements ActionListener {
             setVisible(false);
             try {
                 new frmBooking();
-            } catch (SQLException e1) {
+            } catch (Exception e1) {
                 e1.printStackTrace();
+            }
+        }
+        if(command == "Airport"){
+            try {
+                new frmAirport();
+            }catch (Exception e2){
+                e2.printStackTrace();
             }
         }
     }
